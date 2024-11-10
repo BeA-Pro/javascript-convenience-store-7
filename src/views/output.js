fa9df6023d212bfeabb794787beba0fce8540266
YongHyunKing <yhkim8246@gmail.com>
2024-11-11 01:55:20 +0900

    fix: 요구조건에 맞게 상품 재고 정보 출력하기

--- src/views/output.js
+++ src/views/output.js
@@ -10,7 +10,7 @@ const changeNullPromotion = (promotion) => {
 const OutputView = {
   printProducts(products) {
     Console.print('안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n');
-    products.forEach(({ name, price, quantity, promotion }) => {
+    products.forEach(({ name, quantity, price, promotion }) => {
       Console.print(`- ${name} ${price.toLocaleString()}원 ${changeZeroPrice(quantity)} ${changeNullPromotion(promotion)}`);
     });
     Console.print('');
